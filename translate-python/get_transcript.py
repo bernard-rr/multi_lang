57057f4d383a99e45bafd54e64009eaa23ffb158
Chidi-Bernard Emmanuel <89081878+bernard-rr@users.noreply.github.com>
2023-10-10 09:15:19 +0000

    updated get_transcript

--- translate-python/get_transcript.py
+++ translate-python/get_transcript.py
@@ -1,15 +1,11 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 import re
 
-# transcript = YouTubeTranscriptApi.get_transcript("https://www.youtube.com/watch?v=3jGYHuBrYYQ")
-
-# print(transcript)
-
 def get_youtube_video_id(url):
     # Extract video id using regular expression
-    video_id_match = re.search(r'(?<=v=)[^&#]+', url)
+    video_id_match = re.search(r'(?:v=|\/)([A-Za-z0-9_-]{11})', url)
     if video_id_match:
-        return video_id_match.group(0)
+        return video_id_match.group(1)
     return None
 
 def get_transcript_from_url(url):
@@ -22,7 +18,6 @@ def get_transcript_from_url(url):
     else:
         raise ValueError("Invalid YouTube URL")
 
-# Example usage:
-# url = "https://www.youtube.com/watch?v=3jGYHuBrYYQ"
+# # Example usage:
+# url = "https://m.youtube.com/watch?si=RExS6uPuY3JB2YSi&v=ZekOplAHwqs&feature=youtu.be"
 # print(get_transcript_from_url(url))
-
